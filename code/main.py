ccadb880c16b43c55bb8f9765aa24c20f4609a73
Samuel-Benson <samuelgbens@gmail.com>
2021-10-05 14:37:52 -0600

    I made a change.

---
+++ code/main.py
@@ -0,0 +1,13 @@
+#place function imports below
+from palmer import *
+import heydorn as heyd
+from benson import *
+
+def main():
+    print("Welcome to our collabrative program.")
+    helloFromSamPalmer()
+    heyd.hi_from_ben_heydorn()
+    hi_from_sam_benson()
+
+if __name__ == "__main__":
+    main()
